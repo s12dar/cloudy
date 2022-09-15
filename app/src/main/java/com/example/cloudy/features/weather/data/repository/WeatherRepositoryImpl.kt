87a0f968d35bcabd89a4153c3278c989287d79e3
pserdax <parahatgeldiyew.s@gmail.com>
2022-09-15 00:35:25 -0500

    extended dao

---
+++ app/src/main/java/com/example/cloudy/features/weather/data/repository/WeatherRepositoryImpl.kt
@@ -0,0 +1,4 @@
+package com.example.cloudy.features.weather.data.repository
+
+class WeatherRepositoryImpl {
+}
---
+++ app/src/main/java/com/example/cloudy/features/weather/di/WeatherDataSourceModule.kt
@@ -0,0 +1,4 @@
+package com.example.cloudy.features.weather.di
+
+class WeatherDataSourceModule {
+}
