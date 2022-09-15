3f08eb676924c7233432677952e63b1cd5ac3c92
pserdax <parahatgeldiyew.s@gmail.com>
2022-09-14 21:39:43 -0500

    implemantations of local db and dao

--- app/src/main/java/com/example/cloudy/features/weather/data/local/dao/WeatherDao.kt
+++ app/src/main/java/com/example/cloudy/features/weather/data/local/dao/WeatherDao.kt
@@ -1,4 +1,12 @@
 package com.example.cloudy.features.weather.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import com.example.cloudy.features.weather.data.local.entity.WeatherEntity
+
+@Dao
 interface WeatherDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertWeather(weatherData: WeatherEntity)
 }
--- app/src/main/java/com/example/cloudy/features/weather/di/WeatherDatabaseModule.kt
+++ app/src/main/java/com/example/cloudy/features/weather/di/WeatherDatabaseModule.kt
@@ -1,4 +1,10 @@
 package com.example.cloudy.features.weather.di
 
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
 class WeatherDatabaseModule {
 }
