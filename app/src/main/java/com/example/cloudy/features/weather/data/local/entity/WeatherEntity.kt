8fb31a8cf33af06eb457591fece9457f2d3405f5
pserdax <parahatgeldiyew.s@gmail.com>
2022-09-15 20:44:02 -0500

    add primary key field in an entity

--- app/src/main/java/com/example/cloudy/features/weather/data/local/entity/WeatherEntity.kt
+++ app/src/main/java/com/example/cloudy/features/weather/data/local/entity/WeatherEntity.kt
@@ -2,11 +2,19 @@ package com.example.cloudy.features.weather.data.local.entity
 
 import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @Entity(tableName = "weather_table")
 @JsonClass(generateAdapter = true)
 data class WeatherEntity(
+
+    @PrimaryKey(autoGenerate = true)
+    @field:Json(name = "uid")
+    val uid: Int,
+
     @Embedded
+    @field:Json(name = "weatherData")
     val weatherData: WeatherDataEntity
 )
