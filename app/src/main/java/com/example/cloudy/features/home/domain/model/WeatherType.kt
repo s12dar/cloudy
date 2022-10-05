1ed2d73d445edc6c47f404631ab1e36d696d658f
pserdax <parahatgeldiyew.s@gmail.com>
2022-10-05 10:42:15 -0500

    Added sealed class with weather types

---
+++ app/src/main/java/com/example/cloudy/features/home/domain/model/WeatherType.kt
@@ -0,0 +1,144 @@
+package com.example.cloudy.features.home.domain.model
+
+import androidx.annotation.DrawableRes
+import com.example.cloudy.R
+
+sealed class WeatherType(
+    weatherDesc: String,
+    @DrawableRes val iconRes: Int
+) {
+    object ClearSky : WeatherType(
+        weatherDesc = "Clear Sky",
+        iconRes = R.drawable.ic_clear_sky
+    )
+
+    object MainlyClear : WeatherType(
+        weatherDesc = "Mainly Clear",
+        iconRes = R.drawable.ic_mainly_clear
+    )
+
+    object PartlyCloudy : WeatherType(
+        weatherDesc = "Partly cloudy",
+        iconRes = R.drawable.ic_cloudy
+    )
+
+    object Overcast : WeatherType(
+        weatherDesc = "Overcast",
+        iconRes = R.drawable.ic_cloudy
+    )
+
+    object Foggy : WeatherType(
+        weatherDesc = "Foggy",
+        iconRes = R.drawable.ic_very_cloudy
+    )
+
+    object DepositingRimeFog : WeatherType(
+        weatherDesc = "Depositing rime fog",
+        iconRes = R.drawable.ic_very_cloudy
+    )
+
+    object LightDrizzle : WeatherType(
+        weatherDesc = "Light drizzle",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object ModerateDrizzle : WeatherType(
+        weatherDesc = "Moderate drizzle",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object DenseDrizzle : WeatherType(
+        weatherDesc = "Dense drizzle",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object LightFreezingDrizzle : WeatherType(
+        weatherDesc = "Slight freezing drizzle",
+        iconRes = R.drawable.ic_snowyrainy
+    )
+
+    object DenseFreezingDrizzle : WeatherType(
+        weatherDesc = "Dense freezing drizzle",
+        iconRes = R.drawable.ic_snowyrainy
+    )
+
+    object SlightRain : WeatherType(
+        weatherDesc = "Slight rain",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object ModerateRain : WeatherType(
+        weatherDesc = "Rainy",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object HeavyRain : WeatherType(
+        weatherDesc = "Heavy rain",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object HeavyFreezingRain : WeatherType(
+        weatherDesc = "Heavy freezing rain",
+        iconRes = R.drawable.ic_snowyrainy
+    )
+
+    object SlightSnowFall : WeatherType(
+        weatherDesc = "Slight snow fall",
+        iconRes = R.drawable.ic_snowy
+    )
+
+    object ModerateSnowFall : WeatherType(
+        weatherDesc = "Moderate snow fall",
+        iconRes = R.drawable.ic_snowy
+    )
+
+    object HeavySnowFall : WeatherType(
+        weatherDesc = "Heavy snow fall",
+        iconRes = R.drawable.ic_heavy_snow
+    )
+
+    object SnowGrains : WeatherType(
+        weatherDesc = "Snow grains",
+        iconRes = R.drawable.ic_heavy_snow
+    )
+
+    object SlightRainShowers : WeatherType(
+        weatherDesc = "Slight rain showers",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object ModerateRainShowers : WeatherType(
+        weatherDesc = "Moderate rain showers",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object ViolentRainShowers : WeatherType(
+        weatherDesc = "Violent rain showers",
+        iconRes = R.drawable.ic_rainshower
+    )
+
+    object SlightSnowShowers : WeatherType(
+        weatherDesc = "Light snow showers",
+        iconRes = R.drawable.ic_snowy
+    )
+
+    object HeavySnowShowers : WeatherType(
+        weatherDesc = "Heavy snow showers",
+        iconRes = R.drawable.ic_snowy
+    )
+
+    object ModerateThunderstorm : WeatherType(
+        weatherDesc = "Moderate thunderstorm",
+        iconRes = R.drawable.ic_storm
+    )
+
+    object SlightHailThunderstorm : WeatherType(
+        weatherDesc = "Thunderstorm with slight hail",
+        iconRes = R.drawable.ic_storm
+    )
+
+    object HeavyHailThunderstorm : WeatherType(
+        weatherDesc = "Thunderstorm with heavy hail",
+        iconRes = R.drawable.ic_storm
+    )
+}
---
+++ archive/readme.txt
@@ -0,0 +1,4 @@
+Iconset: Weatherful (https://www.iconfinder.com/iconsets/weatherful)
+Author: Rasmus Nielsen (https://www.iconfinder.com/rasmusnielsendk)
+License: Free for commercial use (Include link to authors website) ()
+Download date: 2022-10-03
