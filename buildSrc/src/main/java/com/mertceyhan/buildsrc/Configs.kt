c6194b8705c0faa0c71d57fb072a62f46d12dfae
s12dar <parahatgeldiyew.s@gmail.com>
2022-12-18 02:43:30 -0600

    implemented :app module gradle migration

--- buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
@@ -1,4 +1,11 @@
 @file:Suppress("PackageDirectoryMismatch")
 
 object Configs {
+    const val compileSdkVersion = 33
+    const val applicationId = "com.lyvetech.cloudy"
+    const val minSdkVersion = 24
+    const val targetSdkVersion = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
