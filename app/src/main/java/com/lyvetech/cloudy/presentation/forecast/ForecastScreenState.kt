9e72b030c358008cf000c7aa80f65ac7f3d5de69
s12dar <parahatgeldiyew.s@gmail.com>
2023-04-03 23:21:08 -0500

    add forecase view model and screen state impl

--- app/src/main/java/com/lyvetech/cloudy/presentation/forecast/ForecastScreenState.kt
+++ app/src/main/java/com/lyvetech/cloudy/presentation/forecast/ForecastScreenState.kt
@@ -1,4 +1,19 @@
 package com.lyvetech.cloudy.presentation.forecast
 
-class ForecastScreenState {
+import com.lyvetech.cloudy.data.pref.AppPreferences
+import com.lyvetech.cloudy.data.pref.TempUnitSelection
+import com.lyvetech.cloudy.data.pref.ThemeSelection
+import com.lyvetech.cloudy.domain.model.WeatherInfo
+
+data class ForecastScreenState(
+    val weatherInfo: WeatherInfo? = null,
+    val appPreferences: AppPreferences = appPreferencesInitialState,
+    val isLoading: Boolean = false
+) {
+    companion object {
+        val appPreferencesInitialState = AppPreferences(
+            selectedTheme = ThemeSelection.SYSTEM,
+            selectedTempUnit = TempUnitSelection.CELSIUS
+        )
+    }
 }
--- app/src/main/java/com/lyvetech/cloudy/presentation/forecast/ForecastViewModel.kt
+++ app/src/main/java/com/lyvetech/cloudy/presentation/forecast/ForecastViewModel.kt
@@ -1,2 +1,46 @@
 package com.lyvetech.cloudy.presentation.forecast
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lyvetech.cloudy.domain.pref.PreferencesManager
+import com.lyvetech.cloudy.domain.repository.HomeRepository
+import com.lyvetech.cloudy.domain.service.LocationTracker
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ForecastViewModel @Inject constructor(
+    private val repository: HomeRepository,
+    private val locationTracker: LocationTracker,
+    private val preferencesManager: PreferencesManager
+) : ViewModel() {
+
+    private var _uiState = MutableStateFlow(ForecastScreenState())
+    val uiState: StateFlow<ForecastScreenState> = _uiState.asStateFlow()
+
+    init {
+        getWeatherInfo()
+    }
+
+    internal fun getWeatherInfo() {
+        _uiState.value = _uiState.value.copy(isLoading = true)
+        viewModelScope.launch {
+            locationTracker.getCurrentLocation().data?.let { location ->
+                repository.getWeatherInfo(location).collect { weatherInfoResponse ->
+                    preferencesManager.appPreferences.collect { appPreferences ->
+                        _uiState.value = _uiState.value.copy(
+                            weatherInfo = weatherInfoResponse.data,
+                            appPreferences = appPreferences
+                        )
+                    }
+                }
+            }
+        }
+
+        _uiState.value = _uiState.value.copy(isLoading = false)
+    }
+}
