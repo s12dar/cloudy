04ae4e8387d36031d478279a00e9473453191d55
s12dar <parahatgeldiyew.s@gmail.com>
2023-04-04 22:59:40 -0500

    add shape with rounded corner

--- app/src/main/java/com/lyvetech/cloudy/common/ui/compose/Shapes.kt
+++ app/src/main/java/com/lyvetech/cloudy/common/ui/compose/Shapes.kt
@@ -1,2 +1,8 @@
 package com.lyvetech.cloudy.common.ui.compose
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.unit.dp
+
+@Stable
+val Shape32 = RoundedCornerShape(32.0.dp)
