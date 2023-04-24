72f1cf7c7b43d040d091819a555f08cdce67c4a1
s12dar <parahatgeldiyew.s@gmail.com>
2023-04-24 03:51:01 -0500

    increment project version code

--- buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
@@ -6,7 +6,7 @@ object Configs {
     const val coreUiId = "com.lyvetech.cloudy.core"
     const val minSdkVersion = 24
     const val targetSdkVersion = 33
-    const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionCode = 2
+    const val versionName = "0.1.2"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
