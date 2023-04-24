dc7b6141a0ee6caccb978773de83209c82b7e647
s12dar <parahatgeldiyew.s@gmail.com>
2023-04-24 03:33:23 -0500

    migrate remaining components to material 3

--- app/src/main/java/com/lyvetech/cloudy/presentation/settings/components/LabelledRadioButton.kt
+++ app/src/main/java/com/lyvetech/cloudy/presentation/settings/components/LabelledRadioButton.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
