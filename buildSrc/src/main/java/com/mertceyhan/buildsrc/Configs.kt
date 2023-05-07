1ba334921277e6842ecf39b37afc30ac6bb4779b
s12dar <parahatgeldiyew.s@gmail.com>
2023-05-07 15:59:14 -0500

    increment build number

--- buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
+++ buildSrc/src/main/java/com/mertceyhan/buildsrc/Configs.kt
@@ -6,7 +6,7 @@ object Configs {
     const val coreUiId = "com.lyvetech.cloudy.core"
     const val minSdkVersion = 24
     const val targetSdkVersion = 33
-    const val versionCode = 4
-    const val versionName = "0.1.4"
+    const val versionCode = 5
+    const val versionName = "0.1.5"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
